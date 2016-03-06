c9f4d39e5fa3dc3ef266f52f053f106117b15efc
Justin Hileman <justin@justinhileman.info>
2016-03-06 12:22:52 -0800

    Move initialization to `init.fish`.

---
+++ init.fish
@@ -0,0 +1,7 @@
+# Global `msg` default color and styles.
+set --global msg_color_fg   FFFFFF
+set --global msg_color_bg   normal
+set --global msg_style_url  00FF00 $msg_color_bg -u
+set --global msg_style_dir  FFA500 $msg_color_bg -u
+set --global msg_color_err  FF0000
+set --global msg_color_ok   00FA9A
